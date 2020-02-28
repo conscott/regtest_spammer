86ecc28a33caa041cf2cb69236a985e8f2816113
conscott <conor.r.scott.88@gmail.com>
2020-02-28 11:11:07 +0000

    Fix startup for BSV

--- start_regtest.sh
+++ start_regtest.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
 
 echo "Killing all bitcoind"
-killall bitcoind
+ps -ef | grep bitcoind | grep -v grep | awk '{print $2}' | xargs kill 2&>/dev/null
 
 sleep 2
 
 echo "Starting new daemons"
 
-rm -rf ./minerdir/*
-rm -rf ./spamdir/*
+mkdir -p minerdir spamdir
+rm -rf ./minerdir/* ./spamdir/*
 
 cp miner.conf minerdir/bitcoin.conf
 cp spammer.conf spamdir/bitcoin.conf
 
 BITCOIN_VERSION=$(bitcoind --version | head -n 1)
+BITCOIN_FORK=$(echo "$BITCOIN_VERSION" | awk '{print $2}')
 BITCOIN_MAJOR=$(echo "$BITCOIN_VERSION" | tr '.' '\n' | head -n 2 | tail -n 1)
 
 echo "Using $BITCOIN_VERSION"
 
-# Start mining node
-bitcoind -datadir=./minerdir -fallbackfee='0.00000001' -daemon
-
-# Start spamming node
-bitcoind -datadir=./spamdir -fallbackfee='0.00000001' -daemon
+# Start miner node and spamming node
+if [ "$BITCOIN_FORK" == "SV" ]; then 
+    bitcoind -datadir=./minerdir -fallbackfee='0.00000001' -daemon -excessiveblocksize=0 -maxstackmemoryusageconsensus=0
+    bitcoind -datadir=./spamdir -fallbackfee='0.00000001' -daemon -excessiveblocksize=0 -maxstackmemoryusageconsensus=0
+else
+    bitcoind -datadir=./minerdir -fallbackfee='0.00000001' -daemon
+    bitcoind -datadir=./spamdir -fallbackfee='0.00000001' -daemon
+fi
 
 echo "Connecting nodes..."
 sleep 5
@@ -32,7 +36,7 @@ bitcoin-cli -datadir=./spamdir/ addnode 127.0.0.1:18444 add
 sleep 1
 
 echo "Mining blocks to send to spammer"
-if [[ $BITCOIN_MAJOR -gt 18 ]]; then
+if [[ "$BITCOIN_FORK" == "SV" || $BITCOIN_MAJOR -gt 18 ]]; then
     ADDRESS_MINER=$(bitcoin-cli -datadir=./minerdir/ getnewaddress)
     ADDRESS_SPAMMER=$(bitcoin-cli -datadir=./spamdir/ getnewaddress)
     bitcoin-cli -datadir=./minerdir/ generatetoaddress 101 "$ADDRESS_MINER"
